dbc522519932ef90847d345de73b0d9f5f788753
telamones <vvtatatavv@gmail.com>
2022-09-11 14:52:45 +0300

    Initial commit

--- src/ru/mirea/lab1/Task3.java
+++ src/ru/mirea/lab1/Task3.java
@@ -1,2 +1,18 @@
-package ru.mirea.lab1;public class Task3 {
+package ru.mirea.lab1;
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        System.out.print("enter numbers: ");
+        Scanner sc = new Scanner(System.in);
+        int sum=0, average;
+        for (int i=0; i<5; i++) {
+            array[i] = sc.nextInt();
+            sum += array[i];
+        }
+        average = sum/5;
+        System.out.println("Sum of your numbers: " + sum);
+        System.out.println("Average of your numbers: " + average);
+    }
 }
