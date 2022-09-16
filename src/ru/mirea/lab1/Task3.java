93e6c5ed0582cca8d0b5549cc407c15e4efe9536
telamones <vvtatatavv@gmail.com>
2022-09-16 10:16:08 +0300

    Initial commit

--- src/ru/mirea/lab1/Task3.java
+++ src/ru/mirea/lab1/Task3.java
@@ -1,11 +1,9 @@
 package ru.mirea.lab1;
-import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
         int[] array = {1, 5, 3, 9, -2};
-        System.out.print("enter numbers: ");
-        Scanner sc = new Scanner(System.in);
+
         int sum=0, average;
         for (int i=0; i<5; i++) {
             sum += array[i];
