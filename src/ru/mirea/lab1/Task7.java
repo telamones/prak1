7d1eec0bf13e40909bfc4fa7bcd424a4538e13b1
telamones <vvtatatavv@gmail.com>
2022-09-16 09:20:18 +0300

    Initial commit

--- src/ru/mirea/lab1/Task7.java
+++ src/ru/mirea/lab1/Task7.java
@@ -1,24 +1,25 @@
 package ru.mirea.lab1;
+
 import java.util.Scanner;
 
 public class Task7 {
     public static int fact(int n) {
-            int factorial = 1;
-            for (int i = 1; i < n + 1; i++) {
-                factorial *= i;
-            }
-            return factorial;
+        int factorial = 1;
+        for (int i = 1; i < n + 1; i++) {
+            factorial *= i;
+        }
+        return factorial;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int number = 0;
         System.out.print("enter your number: ");
-        int number = sc.nextInt();
-        if (number>0) {
-            int factorial = fact(number);
-            System.out.println("factorial of your number: "+factorial);
+        while (!sc.hasNextInt() || (sc.hasNextInt() && (number = sc.nextInt()) < 1)) {
+            System.out.println("you don't enter n>=1, please enter n>=1: ");
+            sc.nextLine();
         }
-        else {
-            System.out.println("error, you enter number less than 1");
-        }
+        int factorial = fact(number);
+        System.out.println("factorial of your number: " + factorial);
     }
 }
